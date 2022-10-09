3339144a96f966a3093f810b210b1c898d89f02a
Harkunov Dmytro <harkunovdmytro@gmail.com>
2022-10-09 15:31:04 +0300

    second-var-branch

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let feature__develop_branch = 'feature/develop-branch';
+let feature__develop_branch_second_var = 'feature/develop-branch-second-var';
 
 console.log(feature__develop_branch);
+console.log(feature__develop_branch_second_var);
