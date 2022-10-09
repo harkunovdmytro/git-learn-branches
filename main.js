b3d5955e44aebbb1b431937dec3cf3b88a29542a
Harkunov Dmytro <harkunovdmytro@gmail.com>
2022-10-09 15:31:04 +0300

    added rebase variable

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let feature__develop_branch = 'feature/develop-branch';
 let feature__develop_branch_second_var = 'feature/develop-branch-second-var';
+let feature__develop_branch_rebase = 'feature/develop-branch-rebase';
 
 console.log(feature__develop_branch);
 console.log(feature__develop_branch_second_var);
+console.log(feature__develop_branch_rebase);
